1bbd132f673e89a404f226f5fbd6324b890e00bb
Precious <ikpaprecious2@gmail.com>
2025-08-03 03:25:00 +0100

    initial commit

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,7 @@ export default defineConfig([
         rules: {
             'no-unused-vars': 'warn',
             '@typescript-eslint/no-unused-vars': 'warn',
-            'no-console': 'warn',
+            'no-console': 'off',
         },
     },
     tseslint.configs.recommended,
